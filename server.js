584e5b102ad66ff4e00b4282e9f2d586a1abc506
krish7x <krishnakumar99g@gmail.com>
2021-02-08 14:40:37 +0530

    server changed

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 
-app.get("/hello", (req,res) => {
+app.get("/", (req,res) => {
     res.send("Hello there!!")
 })
 
-app.listen("80", () => {
+app.listen("2000", () => {
     console.log("server is running at the port 8080");
 })
