2e4c046ac42cd1bd445d41573c2f5608ee410c05
krish7x <krishnakumar99g@gmail.com>
2021-02-08 14:18:45 +0530

    http server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/hello", (req,res) => {
     res.send("Hello there!!")
 })
 
-app.listen("8080", () => {
+app.listen("80", () => {
     console.log("server is running at the port 8080");
 })
